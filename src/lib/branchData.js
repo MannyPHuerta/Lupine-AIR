6d80d590bf92eeac6695df34de475bcebcd6ceaa
mannyph2003 <mannyph2003@hotmail.com>
2026-04-15 02:37:36 +0000

    File changes

---
+++ src/lib/branchData.js
@@ -0,0 +1,44 @@
+export const BRANCH_DATA = {
+  "Corpus Christi": {
+    address: "3729 Saratoga Blvd., Corpus Christi, TX 78415",
+    phone: "(361) 855-7368",
+    city: "Corpus Christi",
+    state: "TX",
+    zip: "78415",
+  },
+  "Brownsville": {
+    address: "2134 Central Blvd., Brownsville, TX 78520",
+    phone: "(956) 542-7200",
+    city: "Brownsville",
+    state: "TX",
+    zip: "78520",
+  },
+  "Harlingen": {
+    address: "1014 W. Tyler Ave., Harlingen, TX 78550",
+    phone: "(956) 364-1014",
+    city: "Harlingen",
+    state: "TX",
+    zip: "78550",
+  },
+  "Harlingen Warehouse": {
+    address: "821 W. Jackson St., Harlingen, TX 78550",
+    phone: "(956) 428-7368",
+    city: "Harlingen",
+    state: "TX",
+    zip: "78550",
+  },
+  "McAllen": {
+    address: "1020 E. Business Hwy 83, McAllen, TX 78501",
+    phone: "(956) 630-5222",
+    city: "McAllen",
+    state: "TX",
+    zip: "78501",
+  },
+  "Weslaco": {
+    address: "404 E. 4th St., Weslaco, TX 78596",
+    phone: "(956) 968-7508",
+    city: "Weslaco",
+    state: "TX",
+    zip: "78596",
+  },
+};
