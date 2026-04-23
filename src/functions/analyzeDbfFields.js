13a0390bdbc02983e2ef68f47039aaacd3afd6a4
mannyph2003 <mannyph2003@hotmail.com>
2026-04-23 20:34:20 +0000

    File changes

--- src/functions/analyzeDbfFields.js
+++ src/functions/analyzeDbfFields.js
@@ -1,6 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.25';
 
-// deno-lint-ignore no-undef
+/* deno-lint-ignore no-undef */
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
